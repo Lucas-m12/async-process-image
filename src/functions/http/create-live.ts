234646ff3f4e0205f1ecd344ae7da3fe561fa5b0
Lucas Marinho <lucasmarinhodasilva2@gmail.com>
2026-07-11 00:37:29 -0300

    Update serverless configuration to use Node.js 24.x, add ARM64 architecture, and enable individual packaging. Introduce new 'create-live' function with POST endpoint. Add path mapping in TypeScript configuration for easier imports.

--- src/functions/http/create-live.ts
+++ src/functions/http/create-live.ts
@@ -1,5 +1,5 @@
-import { type APIGatewayProxyEventV2 } from 'aws-lambda';
-import { response } from '../utils/response.js';
+import { response } from '@/utils/response.js';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
   return response(201, { hello: 'lucas' });
