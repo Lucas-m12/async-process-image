14967c482bccd0d2f369e410d965771ad45d1e96
Lucas Marinho <lucasmarinhodasilva2@gmail.com>
2026-07-12 16:30:21 -0300

    Add sharp dependency and implement image processing in process-image handler to generate multiple resolutions of images from S3 events.

--- src/functions/triggers/process-image.ts
+++ src/functions/triggers/process-image.ts
@@ -1,12 +1,44 @@
 import { getS3Object } from "@/utils/get-s3-object.js";
 import { response } from "@/utils/response.js";
 import type { S3Event } from "aws-lambda";
+import sharp from "sharp";
 
 export const handler = async (event: S3Event) => {
   const promises = event.Records.map(async (record) => {
     const file = await getS3Object(
       { bucketName: record.s3.bucket.name,  objectKey: record.s3.object.key}
     );
+    const [hdFile, sdImage, placeholderImage] = await Promise.all([
+      sharp(file)
+        .resize({
+          width: 1280,
+          height: 720,
+          fit: 'contain',
+          background: '#000',
+        })
+        .toFormat('webp', { quality: 80 })
+        .toBuffer(),
+      sharp(file)
+        .resize({
+          width: 640,
+          height: 360,
+          fit: 'contain',
+          background: '#000',
+        })
+        .toFormat('webp', { quality: 80 })
+        .toBuffer(),
+      sharp(file)
+        .resize({
+          width: 124,
+          height: 70,
+          fit: 'contain',
+          background: '#000',
+        })
+        .toFormat('webp', { quality: 80 })
+        .blur(5)
+        .toBuffer(),
+
+    ]);
   });
   await Promise.all(promises);
   return response(200, { ok: true });
