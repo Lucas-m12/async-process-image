a175b4137f34242faf641412b7a5323453f9bc92
Lucas Marinho <lucasmarinhodasilva2@gmail.com>
2026-07-12 02:07:53 -0300

    Add S3 client integration and update create-live function to generate signed upload URLs for images. Include new AWS SDK dependencies for S3 and request presigner.

--- src/functions/http/create-live.ts
+++ src/functions/http/create-live.ts
@@ -1,8 +1,11 @@
 import { dynamoClient } from '@/clients/dynamo-client.js';
+import { s3Client } from '@/clients/s3-client.js';
 import { env } from '@/config/env.js';
 import { extractFileInfo } from '@/utils/extract-file-info.js';
 import { response } from '@/utils/response.js';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { PutCommand } from '@aws-sdk/lib-dynamodb';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import type { APIGatewayProxyEventV2 } from 'aws-lambda';
 import { randomUUID } from 'node:crypto';
 import { z } from 'zod/mini';
@@ -26,8 +29,17 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
       thumbnailKey
     }
   });
+
+  const putObjectCommand = new PutObjectCommand({
+    Bucket: env.LIVES_IMAGES_BUCKET,
+    Key: thumbnailKey
+  });
+  const uploadUrl = await getSignedUrl(
+    s3Client, putObjectCommand, { expiresIn: 600 }
+  );
   await dynamoClient.send(putItemCommand);
-  return response(201, { liveId });
+
+  return response(201, { liveId, uploadUrl });
 }
 
 const schema = z.object({
